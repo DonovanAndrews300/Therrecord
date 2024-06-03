f92b2b9e6ec49d44e1ee64d625ee16a9c351e5e3
Donovan <donovanandrews300@gmail.com>
2024-06-03 13:36:58 -0400

    fixed config files

--- eslint.config.js
+++ eslint.config.js
@@ -1,61 +1,65 @@
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
-import importPlugin from 'eslint-plugin-import';
-import typescript from '@typescript-eslint/eslint-plugin';
-import typescriptParser from '@typescript-eslint/parser';
-import reactNative from 'eslint-plugin-react-native';
+async function createConfig() {
+  const react = await import('eslint-plugin-react');
+  const reactHooks = await import('eslint-plugin-react-hooks');
+  const jsxA11y = await import('eslint-plugin-jsx-a11y');
+  const importPlugin = await import('eslint-plugin-import');
+  const typescript = await import('@typescript-eslint/eslint-plugin');
+  const typescriptParser = await import('@typescript-eslint/parser');
+  const reactNative = await import('eslint-plugin-react-native');
 
-export default [
-  {
-    files: ['**/*.{js,jsx,ts,tsx}'],
-    languageOptions: {
-      parser: typescriptParser,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    plugins: {
-      react,
-      reactHooks,
-      jsxA11y,
-      import: importPlugin,
-      '@typescript-eslint': typescript,
-      'react-native': reactNative,
-    },
-    settings: {
-      react: {
-        version: 'detect',
+  return [
+    {
+      files: ['**/*.{js,jsx,ts,tsx}'],
+      languageOptions: {
+        parser: typescriptParser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
-      'import/resolver': {
-        node: {
-          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      plugins: {
+        react: react.default,
+        reactHooks: reactHooks.default,
+        jsxA11y: jsxA11y.default,
+        import: importPlugin.default,
+        '@typescript-eslint': typescript.default,
+        'react-native': reactNative.default,
+      },
+      settings: {
+        react: {
+          version: 'detect',
+        },
+        'import/resolver': {
+          node: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          },
         },
       },
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        'no-unused-vars': 'off', // Disable no-unused-vars for @typescript-eslint/no-unused-vars
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {
+            argsIgnorePattern: '^_',
+          },
+        ],
+        'import/order': [
+          'error',
+          {
+            groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+            'newlines-between': 'always',
+          },
+        ],
+        semi: ['error', 'always'], // Enforce semicolons
+        indent: ['error', 2], // Enforce 2 spaces for indentation
+        'react-native/no-unused-styles': 'error',
+        'react-native/split-platform-components': 'warn',
+        'react-native/no-inline-styles': 'error',
+        'react-native/no-color-literals': 'error',
+        'react-native/no-raw-text': 'error',
+      },
     },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      'no-unused-vars': 'off', // Disable no-unused-vars for @typescript-eslint/no-unused-vars
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          argsIgnorePattern: '^_',
-        },
-      ],
-      'import/order': [
-        'error',
-        {
-          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-          'newlines-between': 'always',
-        },
-      ],
-      'semi': ['error', 'always'], // Enforce semicolons
-      'indent': ['error', 2], // Enforce 2 spaces for indentation
-      'react-native/no-unused-styles': 'error',
-      'react-native/split-platform-components': 'warn',
-      'react-native/no-inline-styles': 'error',
-      'react-native/no-color-literals': 'error',
-      'react-native/no-raw-text': 'error',
-    },
-  },
-];
+  ];
+}
+
+module.exports = createConfig().then((config) => config);
--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-    preset: 'react-native',
-    setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
-    transformIgnorePatterns: [
-      'node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-native-community|@react-navigation)',
-    ],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    transform: {
-      '^.+\\.[jt]sx?$': 'babel-jest',
-    },
-  };
-  
+  preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-native-community|@react-navigation)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+};
